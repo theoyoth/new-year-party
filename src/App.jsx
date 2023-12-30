c4110f271b95cd8e776f42bebfbee0d724c43752
theoyoth <theobantang1@gmail.com>
2023-12-30 12:35:45 +0800

    enabled sound of fireworks

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { Fireworks } from '@fireworks-js/react'
 import './App.css'
 import useCountdown from './hook/useCountdown'
+import { useRef } from 'react'
 
 function App() {
+  const container = useRef(null)
   const {seconds,minutes,hours,days} = useCountdown()
 
   const d = new Date()
   let dayText = d.toLocaleDateString();
+
+  const toggleSound = () => {
+    container.current.currentOptions.sound.enabled = !container.current.currentOptions.sound.enabled
+  }
   
   return (
     <div>
       <div className='bg-black' />
       {dayText == "01/01/2024" ? (
         <div className='firework-container'>
+          <div onClick={toggleSound} className='btn-sound'>{container.current.currentOptions.sound.enabled == true ? <span>dissabled sound 🔊</span> : <span>enabled sound 🔈</span>}</div>
           <h1 className='big-text'>✨🎇HAPPY NEW YEAR🎇✨</h1>
           <h1 className='big-text'>🎆2024🎆</h1>
           <p className='desc'>wish you have the best year</p>
           <Fireworks
+            ref={container}
             options={
               { opacity: 0.5,
                 sound: {
@@ -27,8 +35,8 @@ function App() {
                     'https://fireworks.js.org/sounds/explosion2.mp3'
                   ],
                   volume: {
-                    min: 2,
-                    max: 8
+                    min: 5,
+                    max: 10
                   }
                 },
                 explosion:5,
@@ -51,7 +59,7 @@ function App() {
         </div>
       ) : (
       <div className='timer-container'>
-        <h1 className='countdown'>COUNTDOWN🎉</h1>
+        <h1 className='countdown'>🎉COUNTDOWN🎉</h1>
         <div className='timer'>
           <div className='box'>
             <p>days</p>
